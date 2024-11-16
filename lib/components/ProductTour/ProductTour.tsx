7eef10c5bbcef8a1a1baaccd52b636c6ec98c5f0
pnm122 <pnmartin02@gmail.com>
2024-11-15 23:31:17 -0500

    Remove highlighting from product tour tooltips

--- lib/components/ProductTour/ProductTour.tsx
+++ lib/components/ProductTour/ProductTour.tsx
@@ -19,19 +19,9 @@ export type ProductTourProps = Pick<
 	| 'skipText'
 	| 'finishText'
 	| 'name'
-	| 'highlight'
 > & {
 	/** Steps of the tour in the order that they are meant to appear */
-	steps: {
-		/** Selector of the element to attach this tooltip to */
-		tooltipOn: ProductTourTooltipProps['on']
-		/** Optional heading to appear above all other content in the tooltip */
-		heading?: ProductTourTooltipProps['heading']
-		/** Main content of the tooltip */
-		body: ProductTourTooltipProps['body']
-		/** The preferred position for this tooltip. It will change positions to fit on-screen if the preferred position doesn't fit. */
-		position: ProductTourTooltipProps['position']
-	}[]
+	steps: Pick<ProductTourTooltipProps, 'on' | 'heading' | 'body' | 'position'>[]
 	/** Step of the tour that is currently visible. Set to undefined/a number outside of the accepted range to hide the tour */
 	activeStep?: number
 	/** Class(es) to add to all tooltips */
@@ -46,14 +36,14 @@ export default function ProductTour({
 }: ProductTourProps) {
 	return (
 		<>
-			{steps.map(({ tooltipOn, heading, body, position }, index) => (
+			{steps.map(({ on, heading, body, position }, index) => (
 				<ProductTourTooltip
 					{...tooltipProps}
 					key={index}
 					active={index === activeStep}
 					heading={heading}
 					body={body}
-					on={tooltipOn}
+					on={on}
 					position={position}
 					n={index}
 					tourLength={steps.length}
--- lib/components/ProductTourTooltip/ProductTourTooltip.tsx
+++ lib/components/ProductTourTooltip/ProductTourTooltip.tsx
@@ -1,4 +1,3 @@
-import { useLayoutEffect, useRef } from 'react'
 import createClasses from '../../utils/createClasses'
 import Tooltip, { TooltipProps } from '../Tooltip/Tooltip'
 import styles from './ProductTourTooltip.module.css'
@@ -49,8 +48,6 @@ export type ProductTourTooltipProps = Omit<TooltipProps, 'ariaLabel' | 'ariaLive
 	 * @default "Finish"
 	 */
 	finishText?: string
-	/** Whether to highlight the element this tooltip is attached to */
-	highlight?: boolean
 }
 
 export default function ProductTourTooltip({
@@ -72,147 +69,95 @@ export default function ProductTourTooltip({
 	nextText,
 	skipText,
 	finishText,
-	highlight,
 	className,
 	...props
 }: ProductTourTooltipProps) {
 	const locationRangeStart =
 		tourLength <= 5 || n <= 2 ? 0 : n >= tourLength - 2 ? tourLength - 5 : n - 2
 
-	// Track initial styles of element the tooltip is attached to
-	// Used to reset to their initial states after changing them
-	const initialPerspective = useRef<string | null>(null)
-	const initialZIndex = useRef<string | null>(null)
-
-	useLayoutEffect(() => {
-		const onElement = document.querySelector<HTMLElement>(props.on)
-		if (!onElement) {
-			return
-		}
-
-		// Set initial values only once
-		// Could break theoretically if `on` changes and the new element has a different perspective/z-index set...
-		if (initialPerspective.current === null && initialZIndex.current === null) {
-			initialPerspective.current = onElement.style.perspective
-			initialZIndex.current = onElement.style.zIndex
-		}
-	}, [props.on])
-
-	useLayoutEffect(() => {
-		if (!highlight) return
-
-		const onElement = document.querySelector<HTMLElement>(props.on)
-		if (!onElement) {
-			return
-		}
-
-		if (props.active) {
-			// create a stacking context using an uncommon property (if not already applied), attempting to not override anything
-			const hasPerspective = getComputedStyle(onElement).perspective !== 'none'
-			if (!hasPerspective) {
-				onElement.style.perspective = '0px'
-			}
-			// max z-index
-			onElement.style.zIndex = '2147483647'
-		} else {
-			onElement.style.perspective = initialPerspective.current ?? ''
-			onElement.style.zIndex = initialZIndex.current ?? ''
-		}
-	}, [props.on, props.active, highlight])
-
 	return (
-		<>
-			{highlight && (
-				<div
-					className={createClasses({
-						[styles['highlight-background']]: true,
-						[styles['highlight-background--active']]: props.active
-					})}
-				></div>
-			)}
-			<Tooltip
-				className={createClasses({
-					...(className ? { [className]: true } : {}),
-					...(props.clearDefaultStyles ? {} : { [styles['tooltip']]: true })
-				})}
-				ariaLabel={`${name}: Step ${n + 1} of ${tourLength}`}
-				// Other roles don't seem to be read aloud?
-				// Ideally this should be polite alerting, but it doesn't seem to work
-				role="alert"
-				{...props}
-			>
-				{heading && <h1 className={styles['tooltip__heading']}>{heading}</h1>}
-				<p className={styles['tooltip__body']}>{body}</p>
-				<div className={styles['tooltip__bottom']}>
-					<div className={styles['tour-location']}>
-						<ul className={styles['tour-location__dots']}>
-							{new Array(Math.min(5, tourLength)).fill(0).map((_, dotIndex) => {
-								const adjustedIndex = locationRangeStart + dotIndex
-								const isSelected = adjustedIndex === n
-								const dotsToLeft = locationRangeStart
-								const dotsToRight = Math.max(0, tourLength - (locationRangeStart + 5))
-								const isSmall =
-									(dotsToLeft === 1 && dotIndex === 0) ||
-									(dotsToLeft >= 2 && dotIndex === 1) ||
-									(dotsToRight === 1 && dotIndex === 4) ||
-									(dotsToRight >= 2 && dotIndex === 3)
-								const isExtraSmall =
-									(dotsToLeft >= 2 && dotIndex === 0) || (dotsToRight >= 2 && dotIndex === 4)
+		<Tooltip
+			className={createClasses({
+				...(className ? { [className]: true } : {}),
+				...(props.clearDefaultStyles ? {} : { [styles['tooltip']]: true })
+			})}
+			ariaLabel={`${name}: Step ${n + 1} of ${tourLength}`}
+			// Other roles don't seem to be read aloud?
+			// Ideally this should be polite alerting, but it doesn't seem to work
+			role="alert"
+			{...props}
+		>
+			{heading && <h1 className={styles['tooltip__heading']}>{heading}</h1>}
+			<p className={styles['tooltip__body']}>{body}</p>
+			<div className={styles['tooltip__bottom']}>
+				<div className={styles['tour-location']}>
+					<ul className={styles['tour-location__dots']}>
+						{new Array(Math.min(5, tourLength)).fill(0).map((_, dotIndex) => {
+							const adjustedIndex = locationRangeStart + dotIndex
+							const isSelected = adjustedIndex === n
+							const dotsToLeft = locationRangeStart
+							const dotsToRight = Math.max(0, tourLength - (locationRangeStart + 5))
+							const isSmall =
+								(dotsToLeft === 1 && dotIndex === 0) ||
+								(dotsToLeft >= 2 && dotIndex === 1) ||
+								(dotsToRight === 1 && dotIndex === 4) ||
+								(dotsToRight >= 2 && dotIndex === 3)
+							const isExtraSmall =
+								(dotsToLeft >= 2 && dotIndex === 0) || (dotsToRight >= 2 && dotIndex === 4)
 
-								return (
-									<li
-										key={adjustedIndex}
-										className={createClasses({
-											[styles['tour-location__dot']]: true,
-											[styles['tour-location__dot--selected']]: isSelected,
-											[styles['tour-location__dot--small']]: isSmall,
-											[styles['tour-location__dot--extra-small']]: isExtraSmall
-										})}
-									/>
-								)
-							})}
-						</ul>
-						<span aria-hidden="true" className={styles['tour-location__number']}>
-							{n + 1}/{tourLength}
-						</span>
-					</div>
-					<div className={styles['buttons']}>
-						{!hideNextButton && n !== tourLength - 1 && (
-							<button
-								className={createClasses({
-									[styles['button--primary']]: !clearButtonStyles,
-									...(nextButtonClassName ? { [nextButtonClassName]: true } : {})
-								})}
-								onClick={() => onNextClicked && onNextClicked()}
-							>
-								{nextText ?? 'Next'}
-							</button>
-						)}
-						{!hideFinishButton && n === tourLength - 1 && (
-							<button
-								className={createClasses({
-									[styles['button--primary']]: !clearButtonStyles,
-									...(finishButtonClassName ? { [finishButtonClassName]: true } : {})
-								})}
-								onClick={() => onFinishClicked && onFinishClicked()}
-							>
-								{finishText ?? 'Finish'}
-							</button>
-						)}
-						{!hideSkipButton && n !== tourLength - 1 && (
-							<button
-								className={createClasses({
-									[styles['button--secondary']]: !clearButtonStyles,
-									...(skipButtonClassName ? { [skipButtonClassName]: true } : {})
-								})}
-								onClick={() => onSkipClicked && onSkipClicked()}
-							>
-								{skipText ?? 'Skip tour'}
-							</button>
-						)}
-					</div>
+							return (
+								<li
+									key={adjustedIndex}
+									className={createClasses({
+										[styles['tour-location__dot']]: true,
+										[styles['tour-location__dot--selected']]: isSelected,
+										[styles['tour-location__dot--small']]: isSmall,
+										[styles['tour-location__dot--extra-small']]: isExtraSmall
+									})}
+								/>
+							)
+						})}
+					</ul>
+					<span aria-hidden="true" className={styles['tour-location__number']}>
+						{n + 1}/{tourLength}
+					</span>
 				</div>
-			</Tooltip>
-		</>
+				<div className={styles['buttons']}>
+					{!hideNextButton && n !== tourLength - 1 && (
+						<button
+							className={createClasses({
+								[styles['button--primary']]: !clearButtonStyles,
+								...(nextButtonClassName ? { [nextButtonClassName]: true } : {})
+							})}
+							onClick={() => onNextClicked && onNextClicked()}
+						>
+							{nextText ?? 'Next'}
+						</button>
+					)}
+					{!hideFinishButton && n === tourLength - 1 && (
+						<button
+							className={createClasses({
+								[styles['button--primary']]: !clearButtonStyles,
+								...(finishButtonClassName ? { [finishButtonClassName]: true } : {})
+							})}
+							onClick={() => onFinishClicked && onFinishClicked()}
+						>
+							{finishText ?? 'Finish'}
+						</button>
+					)}
+					{!hideSkipButton && n !== tourLength - 1 && (
+						<button
+							className={createClasses({
+								[styles['button--secondary']]: !clearButtonStyles,
+								...(skipButtonClassName ? { [skipButtonClassName]: true } : {})
+							})}
+							onClick={() => onSkipClicked && onSkipClicked()}
+						>
+							{skipText ?? 'Skip tour'}
+						</button>
+					)}
+				</div>
+			</div>
+		</Tooltip>
 	)
 }
